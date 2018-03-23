465318ba2b494c164d84effac6ad58ee2eb57f5c
agungor2 <abdulmecitgungor@gmail.com>
2018-03-23 11:01:22 -0400

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -8,14 +8,7 @@ import scipy.io as sc
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-from tqdm import tqdm
 from sklearn.svm import SVC
-from keras.models import Sequential
-from keras.layers.recurrent import LSTM, GRU
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.embeddings import Embedding
-from keras.layers.normalization import BatchNormalization
-from keras.utils import np_utils
 from sklearn import preprocessing, decomposition, model_selection, metrics, pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -23,9 +16,6 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from keras.layers import GlobalMaxPooling1D, Conv1D, MaxPooling1D, Flatten, Bidirectional, SpatialDropout1D
-from keras.preprocessing import sequence, text
-from keras.callbacks import EarlyStopping
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
